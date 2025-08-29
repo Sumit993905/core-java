69f12669644d254483633a4f10efe2a95d824d16
Sumit <officialsumitraj993905@gmail.com>
2025-08-29 10:02:04 +0530

    second commit

---
+++ DoubleArray.java
@@ -0,0 +1,8 @@
+class DoubleArray {
+    public static void main(String[] args) {
+        double[] numbers = {1.1, 2.2, 3.3, 4.4, 5.5};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
